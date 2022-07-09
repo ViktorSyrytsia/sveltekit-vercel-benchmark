2cd7050fd278393e98e10578f7675d182c9daa60
Oleh-Viktor Syrytsia <osyrytsia@mynexuscare.com>
2022-07-09 18:08:09 +0300

    remove db query

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,15 +4,9 @@ import prisma from "../lib/db";
 export const get: RequestHandler = async (req) => {
   const start = Date.now();
 
-  const businesses = await prisma.businesses.findMany();
-
-  if (!businesses) {
-    return { status: 400 };
-  }
-
   return {
     headers: { "Content-Type": "application/json" },
     status: 200,
-    body: { businesses, start },
+    body: { start },
   };
 };
