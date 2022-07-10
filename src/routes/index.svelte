c0b57e391425d9e0da1e5d55f66c3f1d12992729
Oleh-Viktor Syrytsia <osyrytsia@mynexuscare.com>
2022-07-10 10:20:20 +0300

    add env variable

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
-  import { set_svg_attributes } from "svelte/internal";
 
   // @ts-nocheck
   export let start: number;
+  export let business: any;
 </script>
 
 <h1>Welcome to {Date.now() - start}ms</h1>
+<h1>{JSON.stringify(business)}</h1>
 <p>
   Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
 </p>
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,10 +3,11 @@ import prisma from "../lib/db";
 
 export const get: RequestHandler = async (req) => {
   const start = Date.now();
+  const business = await prisma.businesses.findMany();
 
   return {
     headers: { "Content-Type": "application/json" },
     status: 200,
-    body: { start },
+    body: { start, business },
   };
 };
